03232edaf22c91864e8a55e9e143661158d1b4c5
John Gross <mindstorms11@cox.net>
2017-02-06 01:21:35 -0800

    Create solution

---
+++ Essentials/EssentialsPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torch;
+using Torch.API;
+
+namespace Essentials
+{
+    [Plugin("Essentials", "1.0", "cbfdd6ab-4cda-4544-a201-f73efa3d46c0")]
+    public class EssentialsPlugin : TorchPluginBase
+    {
+        /// <inheritdoc />
+        public override void Update()
+        {
+            
+        }
+
+        /// <inheritdoc />
+        public override void Dispose() {}
+    }
+}
