c2b7cd6616e82667fa31a5aef11200d3a8d8b659
Westin Miller <equinoxscripts@gmail.com>
2017-08-30 11:19:57 -0700

    Local dev version is clearly not a standard release.

--- Versioning/AssemblyVersion.cs
+++ Versioning/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.6695.1")]
-[assembly: AssemblyInformationalVersion("1.5.1-8-g8399387")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("v0.0.0-dev")]
