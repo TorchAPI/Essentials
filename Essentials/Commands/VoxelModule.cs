094cbe3962d41b22a65ae4a63593a091e01d5561
John Gross <mindstorms11@cox.net>
2018-02-09 20:17:00 -0800

    Fix voxel reset command

--- Essentials/Commands/VoxelModule.cs
+++ Essentials/Commands/VoxelModule.cs
@@ -4,15 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Sandbox.Engine.Multiplayer;
-using Sandbox.Engine.Voxels;
 using Sandbox.Game.Entities;
-using Sandbox.Game.Replication;
 using Torch.Commands;
-using VRage.Game.ModAPI;
-using VRage.ModAPI;
 using VRage.Network;
 using VRage.Voxels;
-using Parallel = ParallelTasks.Parallel;
 
 namespace Essentials.Commands
 {
@@ -22,20 +17,19 @@ namespace Essentials.Commands
         [Command("reset all", "Resets all voxel maps.")]
         public void ResetAll()
         {
-            var voxelMaps = MyEntities.GetEntities().Select(x => x as IMyVoxelBase);
+            var voxelMaps = MyEntities.GetEntities().OfType<MyVoxelBase>();
 
-            Console.WriteLine(voxelMaps.Count());
             var count = 0;
             //Parallel.ForEach(voxelMaps, map =>
             try
             {
                 foreach (var map in voxelMaps)
                 {
-                    if (map?.StorageName == null || ((MyStorageBase)map.Storage).DataProvider == null)
+                    if (map.StorageName == null || map.Storage.DataProvider == null)
                         continue;
 
                     map.Storage.Reset(MyStorageDataTypeFlags.All);
-                    ((MyReplicationServer)MyMultiplayer.ReplicationLayer).ForceClientRefresh((MyVoxelBase)map);
+                    ((MyReplicationServer)MyMultiplayer.ReplicationLayer).ForceClientRefresh(map);
                     count++;
                 }
             }
