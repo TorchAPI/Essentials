fdb825a580cb1c1b3c5091c5dca7e808a625720f
Brant Martin <rexxar-tc@users.noreply.github.com>
2018-06-11 07:56:19 -0400

    Implement #38 : cleanup voxels with no grids nearby. Change voxel cleanup to inform client to reset the voxel itself instead of replicating the whole map. Add csproj that got missed in last commit

--- Essentials/Commands/VoxelModule.cs
+++ Essentials/Commands/VoxelModule.cs
@@ -5,9 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Sandbox.Engine.Multiplayer;
 using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Character;
 using Torch.Commands;
+using Torch.Mod;
+using Torch.Mod.Messages;
+using VRage.Game.Entity;
 using VRage.Network;
 using VRage.Voxels;
+using Parallel = ParallelTasks.Parallel;
 
 namespace Essentials.Commands
 {
@@ -18,27 +23,64 @@ namespace Essentials.Commands
         public void ResetAll()
         {
             var voxelMaps = MyEntities.GetEntities().OfType<MyVoxelBase>();
+            
+            var resetIds = new List<long>();
+            Parallel.ForEach(voxelMaps, map =>
+                                        {
+                                            try
+                                            {
+                                                if (map.StorageName == null || map.Storage.DataProvider == null)
+                                                    return;
 
-            var count = 0;
-            //Parallel.ForEach(voxelMaps, map =>
-            try
-            {
-                foreach (var map in voxelMaps)
-                {
-                    if (map.StorageName == null || map.Storage.DataProvider == null)
-                        continue;
+                                                map.Storage.Reset(MyStorageDataTypeFlags.All);
+                                                lock (resetIds)
+                                                    resetIds.Add(map.EntityId);
+                                            }
+                                            catch (Exception e)
+                                            {
+                                                Console.WriteLine($"{e.Message}\n{e.StackTrace}");
+                                            }
+                                        });
+            ModCommunication.SendMessageToClients(new VoxelResetMessage(resetIds.ToArray()));
 
-                    map.Storage.Reset(MyStorageDataTypeFlags.All);
-                    ((MyReplicationServer)MyMultiplayer.ReplicationLayer).ForceClientRefresh(map);
-                    count++;
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{e.Message}\n{e.StackTrace}");
-            }
+            Context.Respond($"Reset {resetIds.Count} voxel maps.");
+        }
 
-            Context.Respond($"Reset {count} voxel maps.");
+        [Command("cleanup asteroids", "Resets all asteroids that don't have a grid or player nearby")]
+        public void CleanupAsteroids()
+        {
+            var voxelMaps = MyEntities.GetEntities().OfType<MyVoxelMap>();
+
+            var resetIds = new List<long>();
+
+            Parallel.ForEach(voxelMaps, map =>
+                                        {
+                                            try
+                                            {
+                                                if (map.StorageName == null || map.Storage.DataProvider == null)
+                                                    return;
+
+                                                var s = map.PositionComp.WorldVolume;
+                                                var nearEntities = new List<MyEntity>();
+
+                                                MyGamePruningStructure.GetAllTopMostEntitiesInSphere(ref s, nearEntities);
+
+                                                if(nearEntities.Any(e => e is MyCubeGrid || e is MyCharacter))
+                                                    return;
+
+                                                map.Storage.Reset(MyStorageDataTypeFlags.All);
+                                                lock(resetIds)
+                                                    resetIds.Add(map.EntityId);
+                                            }
+                                            catch (Exception e)
+                                            {
+                                                Console.WriteLine($"{e.Message}\n{e.StackTrace}");
+                                            }
+                                        });
+
+            ModCommunication.SendMessageToClients(new VoxelResetMessage(resetIds.ToArray()));
+
+            Context.Respond($"Reset {resetIds.Count} voxel maps.");
         }
     }
 }
