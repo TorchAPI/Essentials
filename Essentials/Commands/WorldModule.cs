7d32d44c0afe1144a98d7a6399582f2dbc5cc254
John Gross <mindstorms11@cox.net>
2018-07-20 12:04:04 -0700

    Delete empty factions in the identity cleanup commands

--- Essentials/Commands/WorldModule.cs
+++ Essentials/Commands/WorldModule.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Sandbox.Game.Entities;
+using Sandbox.Game.Gui;
+using Sandbox.Game.Multiplayer;
 using Sandbox.Game.World;
+using SpaceEngineers.Game.GUI;
 using Torch.Commands;
 using Torch.Commands.Permissions;
 using VRage.Game.ModAPI;
@@ -17,19 +20,29 @@ namespace Essentials.Commands
         [Permission(MyPromoteLevel.SpaceMaster)]
         public void CleanIdentities(int days)
         {
+            var count = 0;
             var idents = MySession.Static.Players.GetAllIdentities().ToList();
             var cutoff = DateTime.Now - TimeSpan.FromDays(days);
             foreach (var identity in idents)
             {
                 if (identity.LastLoginTime < cutoff)
+                {
+                    MySession.Static.Factions.KickPlayerFromFaction(identity.IdentityId);
                     MySession.Static.Players.RemoveIdentity(identity.IdentityId);
+                    count++;
+                }
             }
+            
+            CleanFactions();
+            Context.Respond($"Removed {count} old identities");
         }
 
         [Command("identity purge", "Remove identities AND the grids they own if they have not logged on in X days.")]
         [Permission(MyPromoteLevel.SpaceMaster)]
         public void PurgeIdentities(int days)
         {
+            var count = 0;
+            var count2 = 0;
             var grids = MyEntities.GetEntities().OfType<MyCubeGrid>().ToList();
             var idents = MySession.Static.Players.GetAllIdentities().ToList();
             var cutoff = DateTime.Now - TimeSpan.FromDays(days);
@@ -37,15 +50,33 @@ namespace Essentials.Commands
             {
                 if (identity.LastLoginTime < cutoff)
                 {
+                    MySession.Static.Factions.KickPlayerFromFaction(identity.IdentityId);
                     MySession.Static.Players.RemoveIdentity(identity.IdentityId);
+                    count++;
                     foreach (var grid in grids)
                     {
                         if (grid.BigOwners.Contains(identity.IdentityId))
+                        {
                             grid.Close();
+                            count2++;
+                        }
                     }
                 }
             }
+            
+            CleanFactions();
+            Context.Respond($"Removed {count} old identities and {count2} grids owned by them.");
+        }
 
+        private void CleanFactions()
+        {
+            foreach (var faction in MySession.Static.Factions.ToList())
+            {
+                if (faction.Value.Members.Count == 0)
+                {
+                    MyFactionCollection.RemoveFaction(faction.Key);
+                }
+            }
         }
     }
 }
