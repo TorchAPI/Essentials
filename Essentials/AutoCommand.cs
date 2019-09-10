47bf34fbd2067102a810a3a1d9ff22d58fe8f2df
Brant Martin <bmartin@onionstorm.net>
2019-09-10 18:22:29 -0400

    Revert "Auto Commands Update Try again (#129)"
    This reverts commit f1e62622dcfe15cc6c48f21dbcc7783583bc4bfc.

--- Essentials/AutoCommand.cs
+++ Essentials/AutoCommand.cs
@@ -25,27 +25,25 @@ namespace Essentials
         private DateTime _nextRun = DateTime.MinValue;
         private DayOfWeek _day = DayOfWeek.All;
         private Trigger _trigger = Trigger.Disabled;
-        private Gtl _comparer = Gtl.LessThan;
         private int _currentStep;
         private string _name;
         private float _triggerRatio;
         private double _triggerCount;
 
-
+        /*
+        [Display(Description = "Enables or disables this command. NOTE: !admin runauto does NOT respect this setting!")]
+        public bool Enabled
+        {
+            get => _enabled;
+            set => SetValue(ref _enabled, value);
+        }
+        */
         [Display(Name = "Trigger", Description ="Choose a trigger for the command")]
         public Trigger CommandTrigger
         {
             get => _trigger;
             set => SetValue(ref _trigger, value);
         }
-        
-        [Display(Name = "Trigger Operator", Description ="Choose a Ratio Comparer for the command")]
-        public Gtl Compare
-        {
-            get => _comparer;
-            set => SetValue(ref _comparer, value);
-        }
-
         
         [Display(Description = "Sets the name of this command. Use this name in conjunction with !admin runauto to trigger the command from ingame or from other auto commands.")]
         public string Name
@@ -122,19 +120,13 @@ namespace Essentials
             if (DateTime.Now < _nextRun)
                 return;
 
-            switch (CommandTrigger)
-            {
-                case Trigger.GridCount:
-                case Trigger.SimSpeed:
-                case Trigger.PlayerCount:
-                    RunNow();
-                    _nextRun = DateTime.Now + _interval;
-                    return;
-                case Trigger.Scheduled when Interval == TimeSpan.Zero.ToString() && DayOfWeek != DayOfWeek.All && DateTime.Now.DayOfWeek != (System.DayOfWeek)(int)DayOfWeek:
+            if(CommandTrigger == Trigger.Scheduled && Interval == TimeSpan.Zero.ToString())
+                if (DayOfWeek != DayOfWeek.All && DateTime.Now.DayOfWeek != (System.DayOfWeek)(int)DayOfWeek)
+                {
                     //adding one day because I can't be bothered to calculate exact interval
                     _nextRun += TimeSpan.FromDays(1);
                     return;
-            }
+                }
 
 
             if (Steps.Count <= 0)
@@ -148,9 +140,11 @@ namespace Essentials
 
             if (_currentStep < Steps.Count) return;
             _currentStep = 0;
-            _nextRun = _scheduledTime != TimeSpan.Zero
-                    ? DateTime.Now.Date + _scheduledTime + TimeSpan.FromDays(1)
-                    : _nextRun = DateTime.Now + _interval;
+            if(CommandTrigger == Trigger.Scheduled && Interval == TimeSpan.Zero.ToString())
+                _nextRun = DateTime.Now.Date + _scheduledTime + TimeSpan.FromDays(1);
+            else if((CommandTrigger != Trigger.Disabled || CommandTrigger != Trigger.Vote) && Interval != TimeSpan.Zero.ToString())
+
+                _nextRun = DateTime.Now + _interval;
         }
 
 
@@ -215,13 +209,6 @@ namespace Essentials
         {
             return $"{Name} : {_trigger.ToString()} : {Steps.Count}";
         }
-    }
-    
-    public enum Gtl
-    {
-        LessThan,
-        GreaterThan,
-        Equal
     }
 
     public enum Trigger
--- Essentials/AutoCommands.cs
+++ Essentials/AutoCommands.cs
@@ -39,8 +39,11 @@ namespace Essentials
                 case Trigger.Disabled:
                     return  false;
                 case Trigger.OnStart:
-                    return Math.Abs((MyAPIGateway.Session.ElapsedPlayTime - TimeSpan.Parse(command.Interval))
-                               .TotalSeconds) < 1;
+                    var a = Math.Max(TimeSpan.Parse(command.Interval).TotalSeconds, 60);
+                    var b = ((ITorchServer)TorchBase.Instance).ElapsedPlayTime;
+                    if  ((a - b.TotalSeconds) <= 1 && (a - b.TotalSeconds > 0))
+                        command.RunNow();
+                    break;
                 case Trigger.Vote:
                     break;
                 case Trigger.Timed:
@@ -48,72 +51,12 @@ namespace Essentials
                 case Trigger.Scheduled:
                     return true;
                 case Trigger.GridCount:
-                    var gridCount = MyEntities.GetEntities().OfType<IMyCubeGrid>().Count();
-                    switch (command.Compare)
-                    {
-                        case GreaterThan:
-                            return gridCount > command.TriggerCount;
-                        case LessThan:
-                            return gridCount < command.TriggerCount;
-                        case Equal:
-                            return Math.Abs(gridCount-command.TriggerCount)<1;
-                        default:
-                            throw new Exception("meh");
-                    }
+                        return Tree.Grids.Count >= command.TriggerCount;
                 case Trigger.PlayerCount:
-                    switch (command.Compare)
-                    {
-                        case GreaterThan:
-                            return MySession.Static.Players.GetOnlinePlayerCount() >= command.TriggerCount;
-                        case LessThan:
-                            return MySession.Static.Players.GetOnlinePlayerCount() <= command.TriggerCount;
-                        default:
-                            throw new Exception("meh");
-                    }
-
+                    return MySession.Static.Players.GetOnlinePlayerCount() >= command.TriggerCount;
                 case Trigger.SimSpeed:
-                    var commandActive = _simSpeedCheck.TryGetValue(command, out var time);
-                    switch (command.Compare)
-                    {
-                        case GreaterThan:
-                            if (commandActive)
-                            {
-                                if ((DateTime.Now - time).TotalSeconds < command.TriggerCount) break;
-                                _simSpeedCheck.Remove(command);
-                                return Math.Min(Sync.ServerSimulationRatio, 1) > command.TriggerRatio;
-                            }
+                    return Math.Min(Sync.ServerSimulationRatio, 1) <= command.TriggerRatio;
 
-                            if (Math.Min(Sync.ServerSimulationRatio, 1) < command.TriggerRatio) break;
-                            _simSpeedCheck.Add(command, DateTime.Now);
-                            break;
-
-                        case LessThan:
-                            if (commandActive)
-                            {
-                                if ((DateTime.Now - time).TotalSeconds < command.TriggerCount) break;
-                                _simSpeedCheck.Remove(command);
-                                return Math.Min(Sync.ServerSimulationRatio, 1) < command.TriggerRatio;
-                            }
-
-                            if (Math.Min(Sync.ServerSimulationRatio, 1) > command.TriggerRatio) break;
-                            _simSpeedCheck.Add(command, DateTime.Now);
-                            break;
-
-                        case Equal:
-                            if (commandActive)
-                            {
-                                if ((DateTime.Now - time).TotalSeconds < command.TriggerCount) break;
-                                _simSpeedCheck.Remove(command);
-                                return (Math.Abs(Sync.ServerSimulationRatio - command.TriggerRatio) <= 0);
-                            }
-
-                            if (Math.Abs(Sync.ServerSimulationRatio - command.TriggerRatio)>0)
-                                break;
-                            _simSpeedCheck.Add(command, DateTime.Now);
-                            break;
-                       }
-                       break;
-                            
                 default:
                     throw new Exception("fuck it");
             }
