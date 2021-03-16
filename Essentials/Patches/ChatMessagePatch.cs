59cbf0bd3a09aee618d1faaa780f1d7e7611fcdc
Bishbash777 <Bishbash777@gmail.com>
2021-03-17 00:49:13 +0100

    Fixed null crash

--- Essentials/Patches/ChatMessagePatch.cs
+++ Essentials/Patches/ChatMessagePatch.cs
@@ -43,10 +43,12 @@ namespace Essentials.Patches {
 
         private static bool OnChatMessageReceived_Server(ref ChatMsg msg) {
             var Account = PlayerAccountData.GetAccount(msg.Author);
-            var Rank = RanksAndPermissions.GetRankData(Account.Rank);
-            if (Rank.DisplayPrefix) {
-                msg.Author = 0;
-                msg.CustomAuthorName = $"{Rank.Prefix}{Account.Player}";
+            if (Account != null) {
+                var Rank = RanksAndPermissions.GetRankData(Account.Rank);
+                if (Rank.DisplayPrefix) {
+                    msg.Author = 0;
+                    msg.CustomAuthorName = $"{Rank.Prefix}{Account.Player}";
+                }
             }
             return true;
         }
