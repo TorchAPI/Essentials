876ca395b049a39b35986bb5394d57eb6f5913cd
Ryoichiro Oka <ryoka0418@gmail.com>
2022-02-22 23:00:51 +0900

    known steam id txt
    # Conflicts:
    #	manifest.xml

---
+++ Essentials/KnownIdsStorage.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Essentials
+{
+    public sealed class KnownIdsStorage
+    {
+        readonly HashSet<ulong> _steamIds;
+        readonly string _filePath;
+
+        public KnownIdsStorage(string filePath)
+        {
+            _filePath = filePath;
+            _steamIds = new HashSet<ulong>();
+        }
+
+        public void Read()
+        {
+            if (!File.Exists(_filePath)) return;
+
+            _steamIds.Clear();
+            foreach (var line in File.ReadAllLines(_filePath))
+            {
+                if (ulong.TryParse(line.Trim(), out var steamId))
+                {
+                    _steamIds.Add(steamId);
+                }
+            }
+        }
+
+        public bool Contains(ulong steamId)
+        {
+            return _steamIds.Contains(steamId);
+        }
+
+        public void Add(ulong steamId)
+        {
+            if (_steamIds.Add(steamId))
+            {
+                var lines = _steamIds.Select(s => $"{s}");
+                File.WriteAllLines(_filePath, lines);
+            }
+        }
+    }
+}
